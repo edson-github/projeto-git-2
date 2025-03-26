758cabc0a83eaf3885b7726be853efb2bf0280db
edson-github <edson.maxiad@gmail.com>
2025-03-26 12:06:33 -0300

    feat: feature 01

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-# exemplo de herança POO
-
-class Animal:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
-
-    def comer(self):
-        print(f"{self.nome} está comendo")
-
-    def dormir(self):
-        print(f"{self.nome} está dormindo")
+print("Esta é a feature 1")
 
 
-class Cachorro(Animal):
-    def latir(self):
-        print(f"{self.nome} está latindo")
-
-
-class Gato(Animal):
-    def miar(self):
-        print(f"{self.nome} está miando")
